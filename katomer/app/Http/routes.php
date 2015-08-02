be9060ececd8abb1fde3c033851383504da02200
yidne <yidnekachew@katomer.com>
2015-08-02 23:07:17 +0200

    db setup

--- katomer/app/Http/routes.php
+++ katomer/app/Http/routes.php
@@ -11,10 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','PagesController@welcome');
 
-Route::get('contact',function() {
-   return view('contact');
-});
